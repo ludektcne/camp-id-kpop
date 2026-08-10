056fc82c7951df51739c99e1e753e0942279ee5d
ludektcne <lud.trojanek@gmail.com>
2026-08-10 20:43:10 +0200

    Add files via upload

--- operators_staff.js
+++ operators_staff.js
@@ -1,103 +1,103 @@
 const operatorsStaff = {
 
         "KP-027": {
-        number: "027",
-        name: "Mirda",
-        nickname: "",
-        age: "",
-        allergy: "",
-        job: "Instruktor",
-        place: "Dlouhá Brtnice",
-        photo: "images/mirda.jpg"
-    },
+    number: "027",
+    name: "Mirda",
+    nickname: "",
+    age: "",
+    allergy: "",
+    job: "Instruktor",
+    place: "Dlouhá Brtnice",
+    photo: "images/Mirda.jpg"
+},
 
-    "KP-028": {
-        number: "028",
-        name: "Nikola",
-        nickname: "",
-        age: "",
-        allergy: "",
-        job: "Instruktor",
-        place: "Dlouhá Brtnice",
-        photo: "images/nikola.jpg"
-    },
+"KP-028": {
+    number: "028",
+    name: "Nikola",
+    nickname: "",
+    age: "",
+    allergy: "",
+    job: "Instruktor",
+    place: "Dlouhá Brtnice",
+    photo: "images/Niki.jpg"
+},
 
-    "KP-029": {
-        number: "029",
-        name: "Kamča",
-        nickname: "",
-        age: "",
-        allergy: "",
-        job: "Instruktor",
-        place: "Dlouhá Brtnice",
-        photo: "images/kamca.jpg"
-    },
+"KP-029": {
+    number: "029",
+    name: "Kamča",
+    nickname: "",
+    age: "",
+    allergy: "",
+    job: "Instruktor",
+    place: "Dlouhá Brtnice",
+    photo: "images/Kamca.jpg"
+},
 
-    "KP-030": {
-        number: "030",
-        name: "Lili",
-        nickname: "",
-        age: "",
-        allergy: "",
-        job: "Instruktor",
-        place: "Dlouhá Brtnice",
-        photo: "images/lili.jpg"
-    },
+"KP-030": {
+    number: "030",
+    name: "Lili",
+    nickname: "",
+    age: "",
+    allergy: "",
+    job: "Instruktor",
+    place: "Dlouhá Brtnice",
+    photo: "images/Lili.jpg"
+},
 
-    "KP-031": {
-        number: "031",
-        name: "Eliška",
-        nickname: "",
-        age: "",
-        allergy: "",
-        job: "Instruktor",
-        place: "Dlouhá Brtnice",
-        photo: "images/eliska.jpg"
-    },
+"KP-031": {
+    number: "031",
+    name: "Eliška",
+    nickname: "",
+    age: "",
+    allergy: "",
+    job: "Instruktor",
+    place: "Dlouhá Brtnice",
+    photo: "images/Eliska instruktor.jpg"
+},
 
-    "KP-032": {
-        number: "032",
-        name: "Nela",
-        nickname: "",
-        age: "",
-        allergy: "",
-        job: "Instruktor",
-        place: "Dlouhá Brtnice",
-        photo: "images/nela.jpg"
-    },
+"KP-032": {
+    number: "032",
+    name: "Nela",
+    nickname: "",
+    age: "",
+    allergy: "",
+    job: "Instruktor",
+    place: "Dlouhá Brtnice",
+    photo: "images/Neli.jpg"
+},
 
-    "KP-033": {
-        number: "033",
-        name: "Luděk",
-        nickname: "",
-        age: "",
-        allergy: "",
-        job: "Instruktor",
-        place: "Dlouhá Brtnice",
-        photo: "images/ludek.jpg"
-    },
+"KP-033": {
+    number: "033",
+    name: "Luděk",
+    nickname: "",
+    age: "",
+    allergy: "",
+    job: "Instruktor",
+    place: "Dlouhá Brtnice",
+    photo: "images/Ludek.jpg"
+},
 
-    "KP-034": {
-        number: "034",
-        name: "Bivoj",
-        nickname: "Maskot",
-        age: "",
-        allergy: "",
-        job: "K-POP Special Forces",
-        place: "Dlouhá Brtnice",
-        photo: "images/bivoj.jpg"
-    },
+"KP-034": {
+    number: "034",
+    name: "Bivoj",
+    nickname: "",
+    age: "",
+    allergy: "",
+    job: "Instruktor",
+    place: "Dlouhá Brtnice",
+    photo: "images/Bivoj.jpg"
+},
 
-    "KP-035": {
-        number: "035",
-        name: "Tazz",
-        nickname: "",
-        age: "",
-        allergy: "",
-        job: "K-POP Special Forces",
-        place: "Dlouhá Brtnice",
-        photo: "images/tazz.jpg"
-    },
+"KP-035": {
+    number: "035",
+    name: "Tazz",
+    nickname: "",
+    age: "",
+    allergy: "",
+    job: "Instruktor",
+    place: "Dlouhá Brtnice",
+    photo: "images/Tazz.jpg"
+},
     "KP-036": {
         number: "036",
         name: "Eliška Kalců",
